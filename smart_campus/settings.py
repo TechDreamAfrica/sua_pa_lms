d704e967245c54a928f101b6dfc4d718b931c009
Tech Dream Africa <techdreamafrica@gmail.com>
2025-08-21 06:43:12 +0000

    added allowed host

--- smart_campus/settings.py
+++ smart_campus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-igm!co99+z%ug)ajbmo+v1g^mw3!st7+(%y20irxib2&1$^5dy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
